dbc32e30bfd6cc4ec43d6ded0298348ba14de41f
Sunjot Kohli <sunjotsingh16@gmail.com>
2019-11-03 02:17:37 -0500

    Github Finder using React Hooks

---
+++ src/context/github/githubReducer.js
@@ -0,0 +1,50 @@
+import {
+    CLEAR_USERS,
+    GET_USER,
+    SET_LOADING,
+    GET_REPOS,
+    SEARCH_USERS
+} from "../types";
+
+
+
+export default (state, action) => {
+    switch(action.type){
+        case SEARCH_USERS:
+            return {
+                ...state,
+                users: action.payload,
+                loading: false
+            }
+
+        case SET_LOADING:
+            return {
+                ...state,
+                loading: true
+            }
+        
+        case GET_USER:
+            return {
+                ...state,
+                user: action.payload,
+                loading: false
+            }
+
+        case CLEAR_USERS:
+            return {
+                ...state,
+                users: [],
+                loading: false
+            }
+
+        case GET_REPOS:
+            return {
+                ...state,
+                repos: action.payload,
+                loading: false
+            }
+
+        default:
+            return state;
+    }
+}
